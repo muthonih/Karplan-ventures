9b5de800c2f9e7ebf5bac20897c309f9ceb8a74d
muthonih <ivygathecha6@gmail.com>
2024-07-07 06:44:40 +0300

    hamburger menu

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 document.addEventListener("DOMContentLoaded", function() {
     const fadeElements = document.querySelectorAll('.fade-in');
 
@@ -18,15 +17,20 @@ document.addEventListener("DOMContentLoaded", function() {
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function(e) {
             e.preventDefault();
-            document.querySelector(this.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth'
-            });
+            const target = document.querySelector(this.getAttribute('href'));
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth'
+                });
+            }
         });
     });
 
     // Mobile Menu Toggle
+    const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
-    document.querySelector('.navbar').addEventListener('click', function() {
+
+    hamburger.addEventListener('click', function() {
         navLinks.classList.toggle('show');
     });
 
